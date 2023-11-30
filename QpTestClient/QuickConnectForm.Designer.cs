e1e2e6305c203d953b42f0bdc32db370e62187d5
张鹏 <scbeta@qq.com>
2023-11-30 15:56:13 +0800

    Release version 2.2.17

--- QpTestClient/QuickConnectForm.Designer.cs
+++ QpTestClient/QuickConnectForm.Designer.cs
@@ -43,46 +43,48 @@
             // txtUrl
             // 
             txtUrl.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            txtUrl.Location = new System.Drawing.Point(118, 58);
-            txtUrl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtUrl.Location = new System.Drawing.Point(150, 75);
+            txtUrl.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             txtUrl.Name = "txtUrl";
-            txtUrl.Size = new System.Drawing.Size(502, 30);
+            txtUrl.Size = new System.Drawing.Size(638, 38);
             txtUrl.TabIndex = 4;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(57, 62);
+            label2.Location = new System.Drawing.Point(73, 80);
+            label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(48, 24);
+            label2.Size = new System.Drawing.Size(66, 31);
             label2.TabIndex = 5;
             label2.Text = "URL:";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(55, 316);
+            label1.Location = new System.Drawing.Point(70, 408);
+            label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(50, 24);
+            label1.Size = new System.Drawing.Size(68, 31);
             label1.TabIndex = 5;
             label1.Text = "密码:";
             // 
             // txtPassword
             // 
             txtPassword.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            txtPassword.Location = new System.Drawing.Point(116, 312);
-            txtPassword.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtPassword.Location = new System.Drawing.Point(148, 403);
+            txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             txtPassword.Name = "txtPassword";
             txtPassword.PasswordChar = '●';
-            txtPassword.Size = new System.Drawing.Size(502, 30);
+            txtPassword.Size = new System.Drawing.Size(638, 38);
             txtPassword.TabIndex = 5;
             // 
             // btnCancel
             // 
-            btnCancel.Location = new System.Drawing.Point(237, 353);
-            btnCancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnCancel.Location = new System.Drawing.Point(302, 456);
+            btnCancel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             btnCancel.Name = "btnCancel";
-            btnCancel.Size = new System.Drawing.Size(115, 36);
+            btnCancel.Size = new System.Drawing.Size(146, 46);
             btnCancel.TabIndex = 101;
             btnCancel.Text = "取消(&C)";
             btnCancel.UseVisualStyleBackColor = true;
@@ -90,10 +92,10 @@
             // 
             // btnOk
             // 
-            btnOk.Location = new System.Drawing.Point(116, 353);
-            btnOk.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            btnOk.Location = new System.Drawing.Point(148, 456);
+            btnOk.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             btnOk.Name = "btnOk";
-            btnOk.Size = new System.Drawing.Size(115, 36);
+            btnOk.Size = new System.Drawing.Size(146, 46);
             btnOk.TabIndex = 100;
             btnOk.Text = "确定(&O)";
             btnOk.UseVisualStyleBackColor = true;
@@ -102,47 +104,49 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new System.Drawing.Point(57, 21);
+            label4.Location = new System.Drawing.Point(73, 27);
+            label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(50, 24);
+            label4.Size = new System.Drawing.Size(68, 31);
             label4.TabIndex = 5;
             label4.Text = "名称:";
             // 
             // txtName
             // 
             txtName.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            txtName.Location = new System.Drawing.Point(118, 17);
-            txtName.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            txtName.Location = new System.Drawing.Point(150, 22);
+            txtName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             txtName.Name = "txtName";
-            txtName.Size = new System.Drawing.Size(502, 30);
+            txtName.Size = new System.Drawing.Size(638, 38);
             txtName.TabIndex = 3;
             // 
             // textBox1
             // 
             textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            textBox1.Location = new System.Drawing.Point(118, 99);
-            textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            textBox1.Location = new System.Drawing.Point(150, 128);
+            textBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.ReadOnly = true;
-            textBox1.Size = new System.Drawing.Size(501, 203);
+            textBox1.Size = new System.Drawing.Size(637, 261);
             textBox1.TabIndex = 102;
-            textBox1.Text = "TCP: qp.tcp://127.0.0.1:3000\r\nUDP: qp.udp://127.0.0.1:3000\r\nWebSocket: qp.ws://127.0.0.1:3000\r\n命名管道: qp.pipe://./Quick.Protocol\r\n串口: qp.serial://COM1";
+            textBox1.Text = "TCP: qp.tcp://127.0.0.1:3000\r\nWebSocket: qp.ws://127.0.0.1:3000\r\n命名管道: qp.pipe://./Quick.Protocol\r\n串口: qp.serial://COM1";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(58, 103);
+            label3.Location = new System.Drawing.Point(74, 133);
+            label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(50, 24);
+            label3.Size = new System.Drawing.Size(68, 31);
             label3.TabIndex = 103;
             label3.Text = "示例:";
             // 
             // QuickConnectForm
             // 
-            AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            ClientSize = new System.Drawing.Size(640, 415);
+            ClientSize = new System.Drawing.Size(815, 536);
             Controls.Add(label3);
             Controls.Add(textBox1);
             Controls.Add(btnCancel);
@@ -153,7 +157,7 @@
             Controls.Add(label4);
             Controls.Add(txtUrl);
             Controls.Add(label2);
-            Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             Name = "QuickConnectForm";
             StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             Text = "快速添加连接";
