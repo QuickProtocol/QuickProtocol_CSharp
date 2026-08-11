111e3b436a0b9dce9f054829e978a15090d7c68b
张鹏 <scbeta@qq.com>
2026-08-11 09:34:42 +0800

    小优化

--- Quick.Protocol/CommandExecuterManager.cs
+++ Quick.Protocol/CommandExecuterManager.cs
@@ -1,64 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
+﻿namespace Quick.Protocol;
 
-namespace Quick.Protocol
+public class CommandExecuterManager
 {
-    public class CommandExecuterManager
+    private Dictionary<string, Func<QpChannel, object, object>> commandExecuterDict = new Dictionary<string, Func<QpChannel, object, object>>();
+
+    /// <summary>
+    /// 获取全部注册的命令请求类型名称
+    /// </summary>
+    public string[] GetRegisterCommandRequestTypeNames() => commandExecuterDict.Keys.ToArray();
+
+    public void Register(string cmdRequestTypeName, Func<QpChannel, object, object> commandExecuter)
     {
-        private Dictionary<string, Func<QpChannel, object, object>> commandExecuterDict = new Dictionary<string, Func<QpChannel, object, object>>();
+        commandExecuterDict[cmdRequestTypeName] = commandExecuter;
+    }
 
-        /// <summary>
-        /// 获取全部注册的命令请求类型名称
-        /// </summary>
-        public string[] GetRegisterCommandRequestTypeNames() => commandExecuterDict.Keys.ToArray();
+    public void Register<TCmdRequest, TCmdResponse>(Func<QpChannel, TCmdRequest, TCmdResponse> commandExecuter)
+        where TCmdRequest : class, new()
+        where TCmdResponse : class, new()
+    {
+        var cmdRequestTypeName = typeof(TCmdRequest).FullName;
+        commandExecuterDict[cmdRequestTypeName] = (handler, request) => commandExecuter(handler, (TCmdRequest)request);
+    }
 
-        public void Register(string cmdRequestTypeName, Delegate commandExecuter)
-        {
-            commandExecuterDict[cmdRequestTypeName] = (handler, request) => commandExecuter.DynamicInvoke(handler, request);
-        }
+    public void Register<TCmdRequest, TCmdResponse>(TCmdRequest request, Func<QpChannel, TCmdRequest, TCmdResponse> commandExecuter)
+        where TCmdRequest : class, IQpCommandRequest<TCmdRequest, TCmdResponse>, new()
+        where TCmdResponse : class, new()
+    {
+        var cmdRequestTypeName = request.GetType().FullName;
+        commandExecuterDict[cmdRequestTypeName] = (handler, req) => commandExecuter(handler, (TCmdRequest)req);
+    }
 
-        public void Register<TCmdRequest, TCmdResponse>(Func<QpChannel, TCmdRequest, TCmdResponse> commandExecuter)
-            where TCmdRequest : class, new()
-            where TCmdResponse : class, new()
-        {
-            var cmdRequestTypeName = typeof(TCmdRequest).FullName;
-            commandExecuterDict[cmdRequestTypeName] = (handler, request) => commandExecuter(handler, (TCmdRequest)request);
-        }
+    /// <summary>
+    /// 执行命令
+    /// </summary>
+    /// <param name="handler"></param>
+    /// <param name="cmdRequestTypeName"></param>
+    /// <param name="cmdRequestModel"></param>
+    /// <returns></returns>
+    public virtual object ExecuteCommand(QpChannel handler, string cmdRequestTypeName, object cmdRequestModel)
+    {
+        if (!CanExecuteCommand(cmdRequestTypeName))
+            throw new IOException($"Command Request Type[{cmdRequestTypeName}] has no executer.");
+        var commandExecuter = commandExecuterDict[cmdRequestTypeName];
+        return commandExecuter(handler, cmdRequestModel);
+    }
 
-        public void Register<TCmdRequest, TCmdResponse>(TCmdRequest request, Func<QpChannel, TCmdRequest, TCmdResponse> commandExecuter)
-            where TCmdRequest : class, IQpCommandRequest<TCmdRequest, TCmdResponse>, new()
-            where TCmdResponse : class, new()
-        {
-            var cmdRequestTypeName = request.GetType().FullName;
-            commandExecuterDict[cmdRequestTypeName] = (handler, req) => commandExecuter(handler, (TCmdRequest)req);
-        }
-
-        /// <summary>
-        /// 执行命令
-        /// </summary>
-        /// <param name="handler"></param>
-        /// <param name="cmdRequestTypeName"></param>
-        /// <param name="cmdRequestModel"></param>
-        /// <returns></returns>
-        public virtual object ExecuteCommand(QpChannel handler, string cmdRequestTypeName, object cmdRequestModel)
-        {
-            if (!CanExecuteCommand(cmdRequestTypeName))
-                throw new IOException($"Command Request Type[{cmdRequestTypeName}] has no executer.");
-            var commandExecuter = commandExecuterDict[cmdRequestTypeName];
-            return commandExecuter(handler, cmdRequestModel);
-        }
-
-        /// <summary>
-        /// 能否执行指定类型的命令
-        /// </summary>
-        /// <param name="cmdRequestTypeName"></param>
-        /// <returns></returns>
-        public virtual bool CanExecuteCommand(string cmdRequestTypeName)
-        {
-            return commandExecuterDict.ContainsKey(cmdRequestTypeName);
-        }
+    /// <summary>
+    /// 能否执行指定类型的命令
+    /// </summary>
+    /// <param name="cmdRequestTypeName"></param>
+    /// <returns></returns>
+    public virtual bool CanExecuteCommand(string cmdRequestTypeName)
+    {
+        return commandExecuterDict.ContainsKey(cmdRequestTypeName);
     }
 }
--- Quick.Protocol/NoticeHandlerManager.cs
+++ Quick.Protocol/NoticeHandlerManager.cs
@@ -1,55 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿namespace Quick.Protocol;
 
-namespace Quick.Protocol
+public class NoticeHandlerManager
 {
-    public class NoticeHandlerManager
+    private Dictionary<string, Action<QpChannel, object>> noticeHandlerDict = new Dictionary<string, Action<QpChannel, object>>();
+
+    /// <summary>
+    /// 获取全部注册的通知类型名称
+    /// </summary>
+    public string[] GetRegisterNoticeTypeNames() => noticeHandlerDict.Keys.ToArray();
+
+    public void Register(string noticeTypeName, Action<QpChannel, object> noticeHandler)
     {
-        private Dictionary<string, Action<QpChannel, object>> noticeHandlerDict = new Dictionary<string, Action<QpChannel, object>>();
+        noticeHandlerDict[noticeTypeName] = noticeHandler;
+    }
 
-        /// <summary>
-        /// 获取全部注册的通知类型名称
-        /// </summary>
-        public string[] GetRegisterNoticeTypeNames() => noticeHandlerDict.Keys.ToArray();
-
-        public void Register(string noticeTypeName, Delegate noticeHandler)
-        {
-            noticeHandlerDict[noticeTypeName] = (handler, notice) => noticeHandler.DynamicInvoke(handler, notice);
-        }
-
-        public void Register<TNotice>(Action<QpChannel, TNotice> noticeHandler)
-            where TNotice : class, new()
-        {
-            var noticeTypeName = typeof(TNotice).FullName;
-            noticeHandlerDict[noticeTypeName] = (handler, notice) => noticeHandler(handler, (TNotice)notice);
-        }
+    public void Register<TNotice>(Action<QpChannel, TNotice> noticeHandler)
+        where TNotice : class, new()
+    {
+        var noticeTypeName = typeof(TNotice).FullName;
+        noticeHandlerDict[noticeTypeName] = (handler, notice) => noticeHandler(handler, (TNotice)notice);
+    }
 
 
-        /// <summary>
-        /// 处理通知
-        /// </summary>
-        /// <param name="handler"></param>
-        /// <param name="noticeTypeName"></param>
-        /// <param name="noticeModel"></param>
-        /// <returns></returns>
-        public virtual void HandleNotice(QpChannel handler, string noticeTypeName, object noticeModel)
-        {
-            if (!CanHandleNoticed(noticeTypeName))
-                return;
-            var noticeHandler = noticeHandlerDict[noticeTypeName];
-            noticeHandler(handler, noticeModel);
-        }
+    /// <summary>
+    /// 处理通知
+    /// </summary>
+    /// <param name="handler"></param>
+    /// <param name="noticeTypeName"></param>
+    /// <param name="noticeModel"></param>
+    /// <returns></returns>
+    public virtual void HandleNotice(QpChannel handler, string noticeTypeName, object noticeModel)
+    {
+        if (!CanHandleNoticed(noticeTypeName))
+            return;
+        var noticeHandler = noticeHandlerDict[noticeTypeName];
+        noticeHandler(handler, noticeModel);
+    }
 
-        /// <summary>
-        /// 能否处理指定类型的通知
-        /// </summary>
-        /// <param name="noticeTypeName"></param>
-        /// <returns></returns>
-        public virtual bool CanHandleNoticed(string noticeTypeName)
-        {
-            return noticeHandlerDict.ContainsKey(noticeTypeName);
-        }
+    /// <summary>
+    /// 能否处理指定类型的通知
+    /// </summary>
+    /// <param name="noticeTypeName"></param>
+    /// <returns></returns>
+    public virtual bool CanHandleNoticed(string noticeTypeName)
+    {
+        return noticeHandlerDict.ContainsKey(noticeTypeName);
     }
 }
